1fc5d395dd758a7f1446d29d16d433bbbc832c01
aetoksoz <aetoksoz@gmail.com>
2020-04-02 01:03:56 +0300

    Card view added

---
+++ CloneTinder/View/CardView.swift
@@ -0,0 +1,84 @@
+//
+//  CardView.swift
+//  TinderClone
+//
+//  Created by Alperen Toksöz on 02.04.2020.
+//  Copyright © 2020 Alperen Toksöz. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    // MARK: - Properties
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "info_icon").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleShowProfile), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        configureGradientLayer()
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor,bottom: bottomAnchor,right: rightAnchor,paddingLeft: 16,paddingBottom: 16,paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right:rightAnchor,paddingRight: 16)
+        
+    }
+    
+    override func layoutSubviews() {
+        gradientLayer.frame = self.frame
+        print("DEBUG: Did layout subviews")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowProfile() {
+        print("DEBUG: Handle profile..")
+    }
+
+    
+    // MARK: - Helpers
+    
+    private func configureGradientLayer() {
+        gradientLayer.colors = [UIColor.clear.cgColor,UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1]
+        layer.addSublayer(gradientLayer)
+    }
+    
+    
+}
