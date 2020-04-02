14430325c2699f080dde370b18d513de7459c359
aetoksoz <aetoksoz@gmail.com>
2020-04-02 14:35:01 +0300

    Model created and show/previous process added

---
+++ CloneTinder/ViewModel/CardViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  CardViewModel.swift
+//  TinderCloneFinal
+//
+//  Created by Alperen Toksöz on 2.04.2020.
+//  Copyright © 2020 Alperen Toksöz. All rights reserved.
+//
+
+import UIKit
+
+class CardViewModel {
+    
+    let user: User
+    let imageURLs: [String]
+    
+    let userInfoText: NSAttributedString
+    private var imageIndex = 0
+    var index: Int { return imageIndex }
+    
+    var imageUrl: URL?
+    
+    init(user: User) {
+        self.user = user
+        
+        let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+        
+        self.userInfoText = attributedText
+
+        self.imageURLs = user.imageURLs
+        self.imageUrl = URL(string: self.imageURLs[0])
+    }
+    
+    func showNextPhoto() {
+        guard imageIndex < imageURLs.count - 1 else { return }
+        imageIndex += 1
+        imageUrl = URL(string: imageURLs[imageIndex])
+    }
+    
+    func showPreviousPhoto() {
+        guard imageIndex > 0 else { return }
+        imageIndex -= 1
+        imageUrl = URL(string: imageURLs[imageIndex])
+    }
+    
+}
