dbef63633ac1c8c7ac6f76277a9242761000efa6
aetoksoz <aetoksoz@gmail.com>
2020-04-05 02:30:15 +0300

    Fetch currentUser, users and cardviews..

--- CloneTinder/API/Service.swift
+++ CloneTinder/API/Service.swift
@@ -10,6 +10,54 @@ import Firebase
 
 struct Service {
     
+    // MARK: - Fetching
+      
+      static func fetchUser(withUid uid: String, completion: @escaping(User) -> Void) {
+          COLLECTION_USERS.document(uid).getDocument { (snapshot, error) in
+              guard let dictionary = snapshot?.data() else { return }
+              let user = User(dictionary: dictionary)
+              completion(user)
+          }
+      }
+      
+      static func fetchUsers(forCurrentUser user: User, completion: @escaping([User]) -> Void) {
+          var users = [User]()
+          
+          let query = COLLECTION_USERS.whereField("age", isGreaterThanOrEqualTo: user.minSeekingAge).whereField("age", isLessThanOrEqualTo: user.maxSeekingAge)
+          
+          fetchSwipes { (swipedUserIDs) in
+              query.getDocuments { (snapshot, error) in
+                  if let error = error {
+                      print("DEBUG: \(error.localizedDescription)")
+                  } else  {
+                      guard let snapshot = snapshot else { return }
+                      snapshot.documents.forEach({ (document) in
+                          let dictionary = document.data()
+                          let user = User(dictionary: dictionary)
+                        
+                          guard user.uid != Auth.auth().currentUser?.uid else { return }
+                          guard swipedUserIDs[user.uid] == nil else { return }
+                          users.append(user)
+                          
+
+                      })
+                      completion(users)
+                  }
+              }
+          }
+      }
+    
+    private static func fetchSwipes(completion: @escaping([String: Bool]) -> Void ) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        COLLECTION_SWIPES.document(uid).getDocument { (snapshot, error) in
+            guard let data = snapshot?.data() as? [String: Bool] else {
+            completion([String:Bool]())
+            return
+        }
+        completion(data)
+        }
+    }
 
     static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
--- CloneTinder/Controller/HomeController.swift
+++ CloneTinder/Controller/HomeController.swift
@@ -12,8 +12,19 @@ import Firebase
 class HomeController: UIViewController {
     
     // MARK: - Properties
+    
+    private var user: User?
+    
+    private var cardViewModels = [CardViewModel]() {
+        didSet {
+            configureCards()
+        }
+    }
+    
     private let topStack = HomeNavigationStackView()
     private let bottomStack = BottomControlsStackView()
+    private var topCardView: CardView?
+    private var cardViews = [CardView]()
     
     private let deckView: UIView = {
         let view = UIView()
@@ -26,14 +37,52 @@ class HomeController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchCurrentUserAndCards()
+        checkIfUserIsLoggedIn()
         configureUI()
     }
     
     // MARK: - API
+        
+        func fetchUsers(forCurrentUser user: User) {
+            Service.fetchUsers(forCurrentUser: user) { (users) in
+               
+            self.cardViewModels = users.map({ CardViewModel(user: $0)})
+
+            }
+        }
+        
+        func fetchCurrentUserAndCards() {
+            guard let uid = Auth.auth().currentUser?.uid else { return }
+            
+            Service.fetchUser(withUid: uid) { (user) in
+                 print("DEBUG: \(user.name)")
+                self.user = user
+                self.fetchUsers(forCurrentUser: user)
+            }
+        }
+        
+        func checkIfUserIsLoggedIn() {
+            if Auth.auth().currentUser == nil {
+                presentLoginController()
+            }
+        }
     
     // MARK: - Selectors
     
     // MARK: - Helpers
+    
+    func configureCards() {
+        cardViewModels.forEach { (cardViewModel) in
+            let cardView = CardView(viewModel: cardViewModel)
+            deckView.addSubview(cardView)
+            cardView.fillSuperview()
+        }
+        
+        cardViews = deckView.subviews.map({ ($0 as? CardView)! })
+        topCardView = cardViews.last
+    }
+    
     
     func configureUI() {
         view.backgroundColor = .white
@@ -51,7 +100,20 @@ class HomeController: UIViewController {
 
     }
     
+    
+    func presentLoginController() {
+        DispatchQueue.main.async {
+            let controller = LoginController()
+            controller.delegate = self
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav,animated: true,completion: nil)
+        }
+    }
+    
 }
+    
+    
     
    // MARK: - BottomControlsStackViewDelegate
 
@@ -67,15 +129,15 @@ extension HomeController: BottomControlsStackViewDelegate {
     func handleRefresh() {
         print("DEBUG: Handle refresh.")
     }
-}
+    
+    }
+    
 
     // MARK: - AuthenticationDelegate
 
 extension HomeController: AuthenticationDelegate {
     func authenticationComplete() {
         dismiss(animated: true, completion: nil)
-
+        fetchCurrentUserAndCards()
     }
 }
-
-
